4c0f88798f16b0aa17130df914932f370cc7c83d
pullCoding <hogin1005@gmail.com>
2024-05-02 03:40:37 +0900

    Add files via upload

---
+++ src/main/java/org/recycle/domain/Member.java
@@ -0,0 +1,15 @@
+package org.recycle.domain;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+    private Timestamp joinDate;
+}
---
+++ src/main/java/org/recycle/domain/Shop.java
@@ -0,0 +1,4 @@
+package org.recycle.domain;
+
+public class Shop {
+}
